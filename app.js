38e08a3b2f4171cc3fc089e448b8694ee1e35e70
IsaiahPariag <159962406+IsaiahPariag@users.noreply.github.com>
2024-02-13 16:14:14 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+document.getElementById('showLove').addEventListener('click', function() {
+    alert('Elyauna, you are my heart, my soul, my treasure, my today, my tomorrow, my forever, and my everything!');
+});
